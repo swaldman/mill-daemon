c1dcceb5400432d08e15bfc4800751ad3c4eec55
Steve Waldman <swaldman@mchange.com>
2024-01-27 23:13:05 -0500

    Initial import.

---
+++ build.sc
@@ -0,0 +1,27 @@
+import mill._, scalalib._, publish._
+
+val MillVersion = "0.11.5"
+
+object MillDaemon extends RootModule with ScalaModule with PublishModule {
+  override def scalaVersion = "2.13.11"
+  override def ivyDeps = Agg(
+    ivy"com.lihaoyi::mill-main:${MillVersion}",
+    ivy"com.lihaoyi::mill-scalalib:${MillVersion}",
+  )
+  override def scalacOptions = T{ Seq("-deprecation") }
+
+  override def artifactName = "mill-daemon"
+  override def publishVersion = T{"0.0.1-SNAPSHOT"}
+  override def pomSettings    = T{
+    PomSettings(
+      description = "A mill module that implements spawning systemd-appropriate forking daemon processes",
+      organization = "com.mchange",
+      url = "https://github.com/swaldman/mill-daemon",
+      licenses = Seq(License.`Apache-2.0`),
+      versionControl = VersionControl.github("swaldman", "mill-daemon"),
+      developers = Seq(
+	Developer("swaldman", "Steve Waldman", "https://github.com/swaldman")
+      )
+    )
+  }
+}
---
+++ src/com/mchange/milldaemon/DaemonModule.scala
@@ -0,0 +1,162 @@
+package com.mchange.milldaemon
+
+import mill._, define._, scalalib._
+import mill.api.{Ctx,Result}
+import mill.define.Command
+import mill.util.Jvm
+
+import scala.util.control.NonFatal
+
+trait DaemonModule extends JavaModule {
+
+  def runDaemonOut : os.ProcessOutput = os.Inherit
+  def runDaemonErr : os.ProcessOutput = os.Inherit
+
+  def runDaemonPidFile : Option[os.Path] = None
+
+  // modified from mill.util.Jvm.runSubprocessWithBackgroundOutputs
+  /**
+   * Runs a JVM subprocess as a freely forked daemon with the given configuration
+   * @param mainClass The main class to run
+   * @param classPath The classpath
+   * @param JvmArgs Arguments given to the forked JVM
+   * @param envArgs Environment variables used when starting the forked JVM
+   * @param workingDir The working directory to be used by the forked JVM
+   * @param daemonOutputs A tuple (stdout,stderr) for the spawned process
+   * @param useCpPassingJar When `false`, the `-cp` parameter is used to pass the classpath
+   *                        to the forked JVM.
+   *                        When `true`, a temporary empty JAR is created
+   *                        which contains a `Class-Path` manifest entry containing the actual classpath.
+   *                        This might help with long classpaths on OS'es (like Windows)
+   *                        which only supports limited command-line length
+   */
+  def runDaemonSubprocess(
+      mainClass: String,
+      classPath: Agg[os.Path],
+      jvmArgs: Seq[String] = Seq.empty,
+      envArgs: Map[String, String] = Map.empty,
+      mainArgs: Seq[String] = Seq.empty,
+      workingDir: os.Path = null,
+      daemonOutputs: Tuple2[os.ProcessOutput, os.ProcessOutput],
+      useCpPassingJar: Boolean = false
+  )(implicit ctx: Ctx): os.SubProcess = {
+    val cp =
+      if (useCpPassingJar && !classPath.iterator.isEmpty) {
+        val passingJar = os.temp(prefix = "run-", suffix = ".jar", deleteOnExit = false)
+        ctx.log.debug(
+          s"Creating classpath passing jar '${passingJar}' with Class-Path: ${classPath.iterator.map(
+              _.toNIO.toUri().toURL().toExternalForm()
+            ).mkString(" ")}"
+        )
+        Jvm.createClasspathPassingJar(passingJar, classPath)
+        Agg(passingJar)
+      } else {
+        classPath
+      }
+    val javaExe = Jvm.javaExe  
+    val args =
+      Vector(javaExe) ++
+        jvmArgs ++
+        Vector("-cp", cp.iterator.mkString(java.io.File.pathSeparator), mainClass) ++
+        mainArgs
+
+    ctx.log.debug(s"Run daemon subprocess with args: ${args.map(a => s"'${a}'").mkString(" ")}")
+    Jvm.spawnSubprocessWithBackgroundOutputs(args, envArgs, workingDir, Some(daemonOutputs))
+  }
+
+  lazy val ProcessPidMethod : java.lang.reflect.Method =
+    try {
+      classOf[Process].getMethod("pid")
+    }
+    catch {
+      case nsme : NoSuchMethodException =>
+        throw new Exception("Pre Java 9 JVMs do not support discovery of process PIDs.", nsme)
+    }    
+
+  private def pid( subProcess : os.SubProcess ) : Long = {
+    val jproc = subProcess.wrapped
+    ProcessPidMethod.invoke( jproc ).asInstanceOf[Long]
+  }
+
+  protected def doRunDaemon(
+      runClasspath: Seq[PathRef],
+      forkArgs: Seq[String],
+      forkEnv: Map[String, String],
+      finalMainClass: String,
+      forkWorkingDir: os.Path,
+      runUseArgsFile: Boolean,
+      daemonOutputs: Tuple2[os.ProcessOutput, os.ProcessOutput],
+      pidFile: Option[os.Path]
+  )(args: String*): Ctx => Result[os.SubProcess] = ctx => {
+    def spawnIt =
+      runDaemonSubprocess(
+        finalMainClass,
+        runClasspath.map(_.path),
+        forkArgs,
+        forkEnv,
+        args,
+        workingDir = forkWorkingDir,
+        daemonOutputs,
+        useCpPassingJar = runUseArgsFile
+      )(ctx)
+
+    try {
+      pidFile match {
+        case Some( path ) if os.exists( path ) =>
+          Result.Failure(s"A file already exists at PID file location ${path}. Please ensure no daemon is currently running, then delete this file.")
+        case Some( path ) if !path.toIO.canWrite() =>
+          Result.Failure(s"Insufficient permission: Cannot write PID file to location ${path}.")
+        case Some( path ) =>
+          val subProcess = spawnIt
+          os.write( path, data = pid(subProcess).toString() + System.lineSeparator() )
+          Result.Success(subProcess)
+        case None =>
+          Result.Success(spawnIt)
+      }
+    }
+    catch {
+      case NonFatal(t) =>
+        Result.Failure("Failed to spawn daemon subprocess: " + t.printStackTrace())
+    }
+  }
+
+  /**
+   * Runs this module's code in the background as a freestanding daemon process.
+   * The process will run indefinitely, until it exits or it is terminated externally.
+   * It can survive termination of the parent mill process.
+   */
+  def runDaemon(args: String*): Command[Unit] = T.command {
+    val ctx = implicitly[Ctx]
+    val rsubp =
+      doRunDaemon(
+        runClasspath   = runClasspath(),
+        forkArgs       = forkArgs(),
+        forkEnv        = forkEnv(),
+        finalMainClass = finalMainClass(),
+        forkWorkingDir = forkWorkingDir(),
+        runUseArgsFile = runUseArgsFile(),
+        daemonOutputs  = ( runDaemonOut, runDaemonErr ),
+        pidFile = runDaemonPidFile
+      )(args: _*)(ctx)
+    rsubp.map( _ => () )
+  }
+
+  /**
+   * Same as `runDaemon`, but lets you specify a main class to run
+   */
+  def runMainDaemon(mainClass: String, args: String*): Command[Unit] = T.command {
+    val ctx = implicitly[Ctx]
+    val rsubp =
+      doRunDaemon(
+        runClasspath   = runClasspath(),
+        forkArgs       = forkArgs(),
+        forkEnv        = forkEnv(),
+        finalMainClass = mainClass,
+        forkWorkingDir = forkWorkingDir(),
+        runUseArgsFile = runUseArgsFile(),
+        daemonOutputs  = ( runDaemonOut, runDaemonErr ),
+        pidFile = runDaemonPidFile
+      )(args: _*)(ctx)
+    rsubp.map( _ => () )
+  }
+}
