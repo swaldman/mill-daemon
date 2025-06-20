a820c8ba1160ae9f0490ba7dad2bdca448a4fd02
Steve Waldman <swaldman@mchange.com>
2025-06-20 09:17:05 +0300

    Stick with logging to stderr in shutdown hook, as logging to libraries is unreliable in that context.

--- util/src/com/mchange/milldaemon/util/PidFileManager.scala
+++ util/src/com/mchange/milldaemon/util/PidFileManager.scala
@@ -3,12 +3,9 @@ package com.mchange.milldaemon.util
 import scala.util.control.NonFatal
 
 object PidFileManager {
-  private val stdErrLogInfo    : String => Unit = msg => System.err.println(s"INFO: ${msg}")
-  private val stdErrLogWarning : String => Unit = msg => System.err.println(s"WARNING: ${msg}")
-
   lazy val location : Option[String] = sys.env.get("MILL_DAEMON_PID_FILE")
   
-  def shutdownHookCarefulDelete(logInfo : String => Unit = stdErrLogInfo, logWarning : String => Unit = stdErrLogWarning) : Option[Thread] = {
+  def shutdownHookCarefulDelete(verbose : Boolean = true) : Option[Thread] = {
     location.map { pidFileLoc =>
       val pidFilePath = os.Path( pidFileLoc )
       new Thread() {
@@ -17,21 +14,25 @@ object PidFileManager {
             if (os.exists(pidFilePath)) {
               val myPid = ProcessHandle.current().pid()
               val fromFilePid = os.read(pidFilePath).trim().toLong
-              if myPid == fromFilePid then
-                logInfo(s"Shutdown Hook: Removing PID file '${pidFilePath}'")
+              if (myPid == fromFilePid) {
+                if (verbose) System.err.println(s"Shutdown Hook: Removing PID file '${pidFilePath}'")
                 os.remove( pidFilePath )
+              }
             }
           }
           catch {
-            case NonFatal(t) =>
-              logWarning("Throwable while executing autoremove PID file shutdown hook.")
-              t.printStackTrace()
+            case NonFatal(t) => {
+              if (verbose) {
+                System.err.println("WARNING: Throwable while executing autoremove PID file shutdown hook.")
+                t.printStackTrace()
+              }
+            }  
           }
         }
       }
     }
   }
-  def installShutdownHookCarefulDelete(logInfo : String => Unit = stdErrLogInfo, logWarning : String => Unit = stdErrLogWarning) : Unit = {
-    shutdownHookCarefulDelete(logInfo, logWarning).foreach( t => Runtime.getRuntime().addShutdownHook(t) )
+  def installShutdownHookCarefulDelete(verbose : Boolean = true) : Unit = {
+    shutdownHookCarefulDelete(verbose).foreach( t => Runtime.getRuntime().addShutdownHook(t) )
   }
 }
